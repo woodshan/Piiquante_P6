c52e104fe7d51f4d2b2bee2f17e333c7396eda15
Shan Pisciotta <shanpisciotta.pro@gmail.com>
2022-12-07 10:32:10 +0100

    Mise en place du parcours utilisateur

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://Woodstock:michel123@cluster0.ekqabj2.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(express.json());
+
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
