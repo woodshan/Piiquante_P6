785cdf71e7d88ddbb2aced0030e5cec5718c0174
Shan Pisciotta <shanpisciotta.pro@gmail.com>
2022-12-07 18:08:54 +0100

    Mise en place du controller like et dislike

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -8,9 +8,11 @@ exports.createSauce = (req, res, next) => {
   const sauce = new Sauce({
     ...sauceObject,
     userId: req.auth.userId,
-    imageUrl: `${req.protocol}://${req.get("host")}/images/${
-      req.file.filename
-    }`,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: [],
+    usersDisliked: [],
   });
 
   sauce
@@ -44,7 +46,7 @@ exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({_id: req.params.id})
         .then((sauce) => {
             if(sauce.userId != req.auth.userId) {
-                res.status(401).json({message : "Not authorized"});
+                res.status(403).json({message : "unauthorized request"});
             } else {
                 const filename = sauce.imageUrl.split("/images/")[1];
                 fs.unlink(`images/${filename}`, () => {
@@ -67,4 +69,67 @@ exports.getAllSauces = (req, res, next) => {
   Sauce.find()
     .then((sauces) => res.status(200).json(sauces))
     .catch((error) => res.status(400).json({ error }));
+};
+
+exports.likeSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+
+      if(!sauce.usersLiked.includes(req.body.userId) && req.body.like === 1) {
+
+      Sauce.updateOne(
+        { _id: req.params.id },
+        {
+          $inc: {likes: 1},
+          $push: {usersLiked: req.body.userId}
+        }
+      )
+      
+      .then(() => res.status(201).json({message: "L'utilisateur a aimé"}))
+      .catch((error) => res.status(400).json({error}));
+      };
+
+      if(sauce.usersLiked.includes(req.body.userId) && req.body.like === 0) {
+
+        Sauce.updateOne(
+          { _id: req.params.id },
+          {
+            $inc: {likes: -1},
+            $pull: {usersLiked: req.body.userId}
+          }
+        )
+        
+        .then(() => res.status(201).json({message: "L'utilisateur est neutre"}))
+        .catch((error) => res.status(400).json({error}));
+      };
+
+      if(!sauce.usersDisliked.includes(req.body.userId) && req.body.like === -1) {
+
+        Sauce.updateOne(
+          { _id: req.params.id },
+          {
+            $inc: {dislikes: 1},
+            $push: {usersDisliked: req.body.userId}
+          }
+        )
+        
+        .then(() => res.status(201).json({message: "L'utilisateur n'aime pas"}))
+        .catch((error) => res.status(400).json({error}));
+      };
+
+      if(sauce.usersDisliked.includes(req.body.userId) && req.body.like === 0) {
+
+        Sauce.updateOne(
+          { _id: req.params.id },
+          {
+            $inc: {dislikes: -1},
+            $pull: {usersDisliked: req.body.userId}
+          }
+        )
+        
+        .then(() => res.status(201).json({message: "L'utilisateur est neutre"}))
+        .catch((error) => res.status(400).json({error}));
+      };
+    })
+    .catch((error) => res.status(404).json({error}));
 };
--- routes/sauce.js
+++ routes/sauce.js
@@ -6,10 +6,12 @@ const multer = require("../middleware/multer-config");
 
 const sauceCtrl = require("../controllers/sauce");
 
+// Etapes du CRUD
 router.post("/", auth, multer, sauceCtrl.createSauce);
 router.put("/:id", auth, multer, sauceCtrl.modifySauce);
 router.delete("/:id", auth, sauceCtrl.deleteSauce);
 router.get("/:id", auth, sauceCtrl.getOneSauce);
 router.get("/", auth, sauceCtrl.getAllSauces);
+router.post("/:id/like", auth, sauceCtrl.likeSauce);
 
 module.exports = router;
